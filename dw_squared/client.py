56e6e78d19869289f1ac4f5ef9717e798c6938c5
qx6145 <loic.balland@engie.com>
2021-10-13 14:39:25 +0200

    publish test graph, seasonal chart fully parametrized

--- dw_squared/client.py
+++ dw_squared/client.py
@@ -1,5 +1,3 @@
-from typing import Union, List, Any
-import requests as r
 from datetime import datetime as dt
 
 import pandas as pd
@@ -11,22 +9,181 @@ from dw_squared.transform import (
 
 
 class DWSquared():
-    def __init__(self, token=None):
+    def __init__(self, height: int = 600, width: int = 600, token=None):
         self.dw = datawrapper.Datawrapper(access_token=token)
-    
+        self.height, self.width = height, width
+        self._chart = None
+
     @property
-    def today():
+    def default_publish(self):
+        return {
+            'publish': {
+                'blocks': {
+                    'download-image': True,
+                    'logo': False,
+                },
+                'chart-height': self.height,
+                'chart-width': self.width
+            }
+        }
+
+    @property
+    def today_line(self):
+        today_str = self.today.strftime('%d/%m/%Y %H:%M')
+        return {
+            'range-annotations': [
+                    {
+                        'x0': today_str,
+                        'x1': today_str,
+                        'type': 'x',
+                        'color': '#00344c',
+                        'display': 'line',
+                        'opacity': 28,
+                        'strokeType': 'solid',
+                        'strokeWidth': 1
+                    }
+                ],
+        }
+
+    @property
+    def today(self):
         now = dt.utcnow()
-        return dt(now.year,now.month, now.day) 
+        return dt(now.year, now.month, now.day)
 
     def get_charts(self, search='', *args, **kwargs):
         return self.dw.get_charts(search=search, *args, **kwargs)
 
 
-class SWSquaredSeasonal(DWSquared):
-    def __init__(self, token=None):
-        super().__init__(token)
-    
-    def dw_create_plot(self, title, seasonal_data:pd.DataFrame):
-        self.dw.create_chart(title, chart_type='d3-lines', data=seasonal_data)
-                
+class Seasonal(DWSquared):
+
+    def __init__(self,
+                 series: pd.Series,
+                 title: str = '',
+                 source: str = '',
+                 prefix_unit: str = '',
+                 notes: str = '',
+                 freq_graph: str = 'D',
+                 aggregation_freq_graph: str = 'mean',
+                 cutoff_year: int = None,
+                 height: int = None,
+                 width: int = None,
+                 token: str = None,
+                 ):
+        super().__init__(height, width, token)
+        self.series = series
+        self.title = title
+        self.source = source
+        self.notes = notes
+        self.prefix_unit = prefix_unit
+        self.freq_graph = freq_graph
+        self.aggregation_freq_graph = aggregation_freq_graph
+        self.cutoff_year = cutoff_year
+        if self.cutoff_year is None:
+            self.cutoff_year = self.today.year
+        _data = generate_seasonal_frame(
+            self.series, self.freq_graph, self.aggregation_freq_graph)
+        self._data_stats, self.columns_definition = compute_seasonal_stats(
+            _data, cutoff_year=self.cutoff_year)
+        self._data_stats = self._data_stats.reset_index()
+
+    @property
+    def chart(self):
+        if self._chart is None:
+            self._chart = self.dw.create_chart(
+                self.title, chart_type='d3-lines', data=self._data_stats)
+        return self._chart
+
+    def description(self):
+        self.dw.update_description(
+            self.chart['id'],
+            source_name=self.source
+        )
+
+    def metadata(self):
+        name = self._data_stats.columns[1]
+        self.columns_definition['min']['name']
+        properties = {
+            'data': {
+                'annotate': {
+                    'notes': self.notes,
+                },
+                'describe': {
+                    'source-name': self.source,
+                },
+                'column-format': {
+                    str(name): {
+                        'type': 'auto',
+                        'number-append': f' {self.prefix_unit}',
+                    }
+                }
+            },
+            'visualize': {
+                'x-grid': 'off',
+                'y-grid': 'on',
+                'scale-y': 'linear',
+                'sharing': {
+                    'auto': True,
+                    'enabled': False
+                },
+                'labeling': 'right',
+                'base-color': 7,
+                'label-colors': True,
+                'label-margin': 120,
+                'line-dashes': {
+                    self.columns_definition['mean']['name']: 3
+                },
+                'line-widths': {
+                    self.columns_definition['min']['name']: 3,
+                    self.columns_definition['max']['name']: 3,
+                    self.columns_definition['mean']['name']: 3
+                },
+                'custom-colors': {
+                    self.columns_definition['mean']['name']: '#15607a',
+                    self.columns_definition['max']['name']: '#ffffff',
+                    self.columns_definition['min']['name']: '#ffffff',
+                },
+                'interpolation': 'linear',
+                'show-tooltips': True,
+                'x-tick-format': 'MMMM',
+                'y-grid-format': 'auto',
+                'y-grid-labels': 'outside',
+                'chart-type-set': True,
+                'custom-range-x': [
+                    '',
+                    ''
+                ],
+                'custom-range-y': [
+                    '',
+                    ''
+                ],
+                'custom-ticks-x': '',
+                'custom-ticks-y': '',
+                'connector-lines': True,
+                'tooltip-number-format': '0,0.[00]',
+                'tooltip-use-custom-formats': True,
+                'tooltip-x-format': 'll',
+                'y-grid-subdivide': True,
+                'line-value-labels': True,
+                'custom-area-fills': [
+                    {
+                        'to': self.columns_definition['max']['name'],
+                        'from': self.columns_definition['min']['name'],
+                        'color': '#cccccc',
+                        'opacity': 0.3,
+                        'interpolation': 'linear'
+                    }
+                ],
+            }
+        }
+        properties.update(self.default_publish)
+        properties['visualize'].update(self.today_line)
+        self.dw.update_metadata(self.chart['id'], properties)
+
+    def publish(self):
+        self.description()
+        self.metadata()
+        search = self.dw.get_charts(search=self.title)
+        for s in search:
+            self.dw.delete_chart(s['id'])
+        self.dw.publish_chart(self.chart['id'])
+        print(self.chart['id'])
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,10 @@
 import pytest
+from datetime import datetime as dt
 
-from dw_squared.client import DWSquared
+import numpy as np
+import pandas as pd
+
+from dw_squared.client import DWSquared, Seasonal
 
 
 @pytest.fixture
@@ -18,8 +22,20 @@ def test_dwsquared_ping(dw):
     assert [x in list(info.keys()) for x in ('email', 'id')]
 
 
-def test_dwsquared_search(dw):
-    charts = dw.get_charts(published=False)
-    for ch in charts:
-        print(ch)
-    assert False
+def test_seasonal(token):
+    range_date = pd.date_range(
+        start=dt(2001, 1, 1), end=dt(2021, 9, 30), freq='D')
+    data = pd.Series(np.cumsum(np.random.randn(len(range_date))),
+                     index=range_date, name='serie')
+    seasonal_plot = Seasonal(
+        series=data,
+        title='test',
+        token=token,
+        source='me',
+        notes='a note',
+        prefix_unit='bananas',
+        cutoff_year=2020,
+    )
+    seasonal_plot.publish()
+    published = seasonal_plot.get_charts(search='test', published=True)
+    assert published[0]['id'] == seasonal_plot._chart['id']
